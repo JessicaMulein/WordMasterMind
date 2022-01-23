034d7c816c31ce82d8c71348aa010e51e640ab1f
Jessica Mulein <jessica@brightchain.org>
2022-01-23 13:36:37 -0800

    cleanup unit test detector and improve performance

--- src/WordMasterMind/Helpers/UnitTestDetector.cs
+++ src/WordMasterMind/Helpers/UnitTestDetector.cs
@@ -4,27 +4,39 @@ namespace WordMasterMind.Helpers;
 
 public static class UnitTestDetector
 {
-    public static readonly HashSet<string> UnitTestAttributes = new()
+    private static bool? _isInUnitTest;
+
+    private static readonly HashSet<string> UnitTestAttributes = new()
     {
         "Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute",
         "NUnit.Framework.TestFixtureAttribute"
     };
 
+    /// <summary>
+    ///     Use reflection to determine if the current assembly is a unit test.
+    ///     Memoization prevents repeated reflection.
+    /// </summary>
+    /// <exception cref="InvalidOperationException"></exception>
     public static bool IsRunningInUnitTest
     {
         get
         {
-            return new StackTrace().GetFrames()
+            _isInUnitTest ??= new StackTrace()
+                .GetFrames()
                 .Any(predicate: f =>
                 {
                     var method = f.GetMethod();
                     if (method == null) return false;
                     var declaringType = method.DeclaringType;
-                    return declaringType != null && declaringType.GetCustomAttributes(inherit: false)
-                        .Any(predicate: x =>
-                            UnitTestAttributes.Contains(item: x.GetType().FullName ??
-                                                              throw new InvalidOperationException()));
+                    return declaringType != null &&
+                           declaringType
+                               .GetCustomAttributes(inherit: false)
+                               .Any(predicate: x =>
+                                   UnitTestAttributes.Contains(item: x.GetType().FullName ??
+                                                                     throw new InvalidOperationException()));
                 });
+
+            return _isInUnitTest.Value;
         }
     }
 }
