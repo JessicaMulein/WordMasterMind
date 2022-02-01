e84e1dc3787b2fc33f1bb9426c027fde20b668d8
Jessica Mulein <jessica@brightchain.org>
2022-01-31 18:08:11 -0800

    start setting up the state machine

--- src/WordMasterMind.Blazor/Components/GameStateMachine.razor.cs
+++ src/WordMasterMind.Blazor/Components/GameStateMachine.razor.cs
@@ -6,6 +6,8 @@ namespace WordMasterMind.Blazor.Components;
 
 public partial class GameStateMachine
 {
+    private GameState _gameState;
+
     public GameStateMachine()
     {
         this.Game = null;
@@ -15,9 +17,63 @@ public partial class GameStateMachine
         this.LiteralDictionary = null;
     }
 
-    public WordMasterMindGame? Game { get; private set; }
+    public Library.Models.WordMasterMindGame? Game { get; private set; }
     public LiteralDictionarySources? DictionarySource { get; private set; }
     public int? WordLength { get; private set; }
     public LiteralDictionary? LiteralDictionary { get; private set; }
-    public GameState State { get; private set; }
+
+    public GameState State
+    {
+        get => this._gameState;
+        private set
+        {
+            var oldState = this._gameState;
+            switch (oldState)
+            {
+                case GameState.Rules:
+                    if (value != GameState.SourceSelection)
+                        throw new Exception(message: "SourceSelection state can only be entered from Rules state");
+                    break;
+                case GameState.SourceSelection:
+                    if (value != GameState.LengthSelection)
+                        throw new Exception(
+                            message: "LengthSelection state can only be entered from SourceSelection state");
+
+                    if (this.DictionarySource is null)
+                        throw new Exception(message: "Cannot enter LengthSelection state without a dictionary source");
+                    break;
+                case GameState.LengthSelection:
+                    if (value != GameState.Playing)
+                        throw new Exception(message: "Playing state can only be entered from LengthSelection state");
+
+                    if (this.WordLength is null)
+                        throw new Exception(message: "Cannot start playing without a word length");
+                    break;
+                case GameState.Playing:
+                    if (value != GameState.GameOver)
+                        throw new Exception(message: "GameOver state can only be entered from Playing state");
+
+                    if (!this.Game.GameOver) throw new Exception(message: "Game is not over!");
+
+                    if (this.Game is null) throw new Exception(message: "Unexpected game state");
+                    break;
+                case GameState.GameOver:
+                    if (value != GameState.Rules)
+                        throw new Exception(message: "Rules state can only be entered from GameOver state");
+
+                    break;
+                default:
+                    throw new Exception(message: $"Unknown game state: {this._gameState}");
+            }
+
+            this._gameState = value;
+            this.OnStateChanged(
+                oldState: oldState,
+                newState: value);
+        }
+    }
+
+    public void OnStateChanged(GameState oldState, GameState newState)
+    {
+    }
 }
