597c13bccd572600772af6864a2d83fd96040900
Jessica Mulein <jessica@brightchain.org>
2022-02-04 09:32:19 -0800

    flesh out gameRow

--- src/WordMasterMind.Blazor/Components/GameRow.razor.cs
+++ src/WordMasterMind.Blazor/Components/GameRow.razor.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components;
+using WordMasterMind.Blazor.Interfaces;
 using WordMasterMind.Library.Helpers;
 
 namespace WordMasterMind.Blazor.Components;
 
 public partial class GameRow
 {
+    [Inject] public IGameStateMachine GameStateMachine { get; set; }
+    [CascadingParameter] public int AttemptNumber { get; set; }
     [CascadingParameter] public string Letters { get; set; } = string.Empty;
     [CascadingParameter] public int Length { get; set; } = Constants.StandardLength;
 }
