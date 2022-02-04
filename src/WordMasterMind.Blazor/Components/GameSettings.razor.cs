d18e8ca6e592964457ec12d844d8213e1b5d3ca4
Jessica Mulein <jessica@brightchain.org>
2022-02-04 09:58:03 -0800

    setting modal

--- src/WordMasterMind.Blazor/Components/GameSettings.razor.cs
+++ src/WordMasterMind.Blazor/Components/GameSettings.razor.cs
@@ -6,4 +6,16 @@ namespace WordMasterMind.Blazor.Components;
 public partial class GameSettings
 {
     [Inject] public IGameStateMachine GameStateMachine { get; set; }
+    
+    [Parameter] public EventCallback<bool> OnClose { get; set; }
+
+    private Task ModalCancel()
+    {
+        return this.OnClose.InvokeAsync(arg: false);
+    }
+
+    private Task ModalOk()
+    {
+        return this.OnClose.InvokeAsync(arg: true);
+    }
 }
