ac846004e8a43c667490bb3646fa4742ce189d60
Jessica Mulein <jessica@brightchain.org>
2022-02-09 00:46:52 -0800

    try to get fancy with forks script

--- setup-personal-forks.sh
+++ setup-personal-forks.sh
@@ -5,28 +5,40 @@ then
   exit 1
 fi
 
-setupFork() {
+setupFork()
+{
   _USERNAME=$1
+  _MODULE=$2
+  _FROMDIR=$3
+  _PATH=$(basename "$_MODULE")
+  echo ""
+  echo "  - entering '${_MODULE}'"
+  cd "${_FROMDIR}" && cd "${_MODULE}"
+  echo "  - before:"
+  git remote -v
+  echo "  - Setting origins for ${_MODULE}"
+  git remote rename origin upstream
+  git remote set-url --push upstream git@github.com:WordMasterMind/${_PATH}.git
+  git remote add origin https://github.com/${_USERNAME}/${_PATH}.git
+  git remote set-url --push origin git@github.com:${_USERNAME}/${_PATH}.git
+  echo ""
+  echo "  - after:"
+  git remote -v
+}
+
+setupForks() {
+  _USERNAME=$1
+  _FROMDIR=$2
   echo ""
   echo "Setting origins for all submodules"
-  _OPWD=$(pwd)
   for MODULE in $_GIT_SUBMODULES; do
-    echo ""
-    echo "  - entering '${MODULE}'"
-    cd "${_OPWD}" && cd "${MODULE}" # avoid os path separator issues
-    echo "  - before:"
-    git remote -v
-    echo "  - Setting origins for ${MODULE}"
-    git remote rename origin upstream
-    git remote set-url --push upstream git@github.com:WordMasterMind/${MODULE}
-    git remote add origin https://github.com/${_USERNAME}/${MODULE}
-    git remote set-url --push origin git@github.com:${_USERNAME}/${MODULE}
-    echo ""
-    echo "  - after:"
-    git remote -v
+    if [[ "${MODULE}" == "wiki" ]]
+    then
+      echo "Skipping wiki"
+      continue
+    fi
+    setupFork "${_USERNAME}" "${MODULE}" "${_FROMDIR}"
   done
-  echo "returning to ${_OPWD}"
-  cd "${_OPWD}"
 }
 
 echo ""
@@ -34,7 +46,9 @@ echo "Set up Personal Fork Origin:"
 echo "-----------------"
 echo "Getting list of modules"
 export _GIT_SUBMODULES=$(git submodule | awk '{ print $2; }')
-setupFork "$1"
+_OPWD=$(pwd)
+setupFork "$1" WordMasterMind ".."
+cd "${_OPWD}" && setupForks "$1" "${_OPWD}"
 echo ""
 echo "-----------------"
 echo "Done"
